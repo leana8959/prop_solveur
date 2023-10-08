8ca6ae3b4b2c39609ade817e792b82d84a32f881
Léana 江 <leana.jiang@icloud.com>
2023-10-08 17:18:22 +0200

    fix: sousFormule, sousFormuleStricte

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,11 @@ module Main where
 import Control.Arrow (Arrow (second))
 import Data.Function (on)
 import Data.List
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
-newtype Proposition = Prop String deriving (Show, Eq)
+type Ident = String
+newtype Proposition = Prop Ident deriving (Show, Eq, Ord)
 
 data Formule
   = Bottom
@@ -14,7 +17,7 @@ data Formule
   | Et Formule Formule
   | Ou Formule Formule
   | Implique Formule Formule
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 hauteur :: Formule -> Int
 hauteur f = case f of
@@ -36,127 +39,55 @@ nombreDeOu f = case f of
   Ou f1 f2 -> 1 + max (hauteur f1) (hauteur f2)
   Implique f1 f2 -> max (hauteur f1) (hauteur f2)
 
-sousFormule :: Formule -> [Formule]
+sousFormule :: Formule -> S.Set Formule
 sousFormule f = case f of
-  Bottom -> [Bottom]
-  Top -> [Top]
-  P p -> [P p]
-  Non f -> Non f : sousFormule f
-  Et f1 f2 -> [Et f1 f2] ++ sousFormule f1 ++ sousFormule f2
-  Ou f1 f2 -> [Ou f1 f2] ++ sousFormule f1 ++ sousFormule f2
-  Implique f1 f2 -> [Implique f1 f2] ++ sousFormule f1 ++ sousFormule f2
-
-sousFormuleStricte :: Formule -> [Formule]
-sousFormuleStricte f = case f of
-  Bottom -> []
-  Top -> []
-  P _ -> []
-  Non f -> f : sousFormule f
-  Et f1 f2 -> [f1, f2] ++ sousFormule f1 ++ sousFormule f2
-  Ou f1 f2 -> [f1, f2] ++ sousFormule f1 ++ sousFormule f2
-  Implique f1 f2 -> [f1, f2] ++ sousFormule f1 ++ sousFormule f2
-
-simp :: Formule -> Formule
-simp Bottom = Bottom
-simp Top = Top
-simp (P p) = P p
-simp (Non f) = case simp f of
-  Bottom -> Top
-  Top -> Bottom
-  f' -> Non f'
-simp (Et f1 f2) = case (simp f1, simp f2) of
-  (Bottom, Bottom) -> Bottom
-  (Bottom, Top) -> Bottom
-  (Top, Bottom) -> Bottom
-  (Top, Top) -> Top
-  (f1', f2') -> Et f1' f2'
-simp (Ou f1 f2) = case (simp f1, simp f2) of
-  (Bottom, Bottom) -> Bottom
-  (Bottom, Top) -> Top
-  (Top, Bottom) -> Top
-  (Top, Top) -> Top
-  (f1', f2') -> Ou f1' f2'
-simp (Implique f1 f2) = case (simp f1, simp f2) of
-  (Bottom, Bottom) -> Top
-  (Bottom, Top) -> Top
-  (Top, Bottom) -> Bottom
-  (Top, Top) -> Top
-  (f1', f2') -> Implique f1' f2'
-
-solve :: Formule -> Maybe [(Proposition, Formule)]
-solve f = case simp f of
-  Bottom -> Nothing
-  Top -> Just []
-  P p -> Just [(p, Bottom), (p, Top)]
-  Non f -> filter ((== Bottom) . snd) <$> solve f
-  Et f1 f2 -> do
-    x <- filter ((== Top) . snd) <$> solve f1
-    y <- filter ((== Top) . snd) <$> solve f2
-    pure $ union x y
+  Bottom -> S.singleton Bottom
+  Top -> S.singleton Top
+  P p -> S.singleton (P p)
+  Non f ->
+    let this = S.singleton (Non f)
+        f' = sousFormule f
+    in  S.union this (sousFormule f)
+  Et f1 f2 ->
+    let this = S.singleton (Et f1 f2)
+        f1' = sousFormule f1
+        f2' = sousFormule f2
+    in  S.union this (S.union f1' f2')
   Ou f1 f2 ->
-    let x = filter ((== Top) . snd) <$> solve f1
-        y = filter ((== Top) . snd) <$> solve f2
-    in  if null x then y else x
+    let this = S.singleton (Ou f1 f2)
+        f1' = sousFormule f1
+        f2' = sousFormule f2
+    in  S.union this (S.union f1' f2')
   Implique f1 f2 ->
-    -- non p ou q
-    let x = filter ((== Bottom) . snd) <$> solve f1
-        y = filter ((== Top) . snd) <$> solve f2
-    in  if null x then y else x
+    let this = S.singleton (Implique f1 f2)
+        f1' = sousFormule f1
+        f2' = sousFormule f2
+    in  S.union this (S.union f1' f2')
 
-exempleNon :: Formule
-exempleNon =
-  Non (P (Prop "a"))
-
-exempleEt :: Formule
-exempleEt =
-  Et
-    (P (Prop "a"))
-    (P (Prop "b"))
-
-exempleOu :: Formule
-exempleOu =
-  Ou
-    (P (Prop "a"))
-    (P (Prop "b"))
-
-exempleImprique :: Formule
-exempleImprique =
-  Et
-    ( Ou
-        (P (Prop "a"))
-        (P (Prop "b"))
-    )
-    ( Et
-        (P (Prop "b"))
-        (P (Prop "c"))
-    )
-
-exempleImprique' :: Formule
-exempleImprique' =
-  Et
-    ( Ou
-        (P (Prop "a"))
-        (P (Prop "b"))
-    )
-    ( Et
-        (P (Prop "b"))
-        Top
-    )
-
-exempleImplique :: Formule
-exempleImplique =
-  Implique
-    (P (Prop "a"))
-    (P (Prop "b"))
-
-exempleImplique' :: Formule
-exempleImplique' =
-  Implique
-    Bottom
-    (P (Prop "b"))
-
--- >>> solve exempleImplique'
--- Just [(Prop "b",Top)]
+sousFormuleStricte :: Formule -> S.Set Formule
+sousFormuleStricte f = case f of
+  Bottom -> S.empty
+  Top -> S.empty
+  P _ -> S.empty
+  Non f ->
+    let this = S.singleton f
+        f' = sousFormuleStricte f
+    in  S.union this f'
+  Et f1 f2 ->
+    let this = S.fromList [f1, f2]
+        f1' = sousFormuleStricte f1
+        f2' = sousFormuleStricte f2
+    in  S.union this (S.union f1' f2')
+  Ou f1 f2 ->
+    let this = S.fromList [f1, f2]
+        f1' = sousFormuleStricte f1
+        f2' = sousFormuleStricte f2
+    in  S.union this (S.union f1' f2')
+  Implique f1 f2 ->
+    let this = S.fromList [f1, f2]
+        f1' = sousFormuleStricte f1
+        f2' = sousFormuleStricte f2
+    in  S.union this (S.union f1' f2')
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
